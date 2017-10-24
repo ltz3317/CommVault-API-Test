133c801831dd955f68d7d1b20867afe73ea91bfc
Eric So <eric@woooa.net>
2017-10-24 16:47:45 +0800

    Divided steps in showall.sh by different agents.

--- showall.sh
+++ showall.sh
@@ -6,13 +6,8 @@ DIR=$(dirname $0)
 cd $DIR
 source ./setenv.sh
 
-## Get client information ##
-
-if [ "$APPNAME" = "Virtual Server" ]
-then
-	echo "Client Name: $CLIENTNAME"
-	export CLIENTID=$($DIR/get_vsa_clientid_by_clientname.sh)
-else
+get_client_info()
+{
 	export CLIENTID=$($DIR/get_clientid_by_clientname.sh)
 	if [ -z "$CLIENTID" ]
 	then
@@ -20,109 +15,189 @@ else
 		echo "Client Name: $CLIENTNAME"
 		export CLIENTID=$($DIR/get_clientid_by_clientname.sh)
 	fi
-fi
-echo "Client ID: $CLIENTID"
+	echo "Client ID: $CLIENTID"
 
-CURLOUT=$(eval $CURLCMD -L $BASEURI"/Client/$CLIENTID")
-echo $CURLOUT | xmlstarlet sel -t -m //dataInterfacePair -o "DIP Active: " -v @active -n -m SourceInterface -o "SourceInterface Client ID: " -v @ClientId -n -o "Interface: " -v @Interface -n -m ../DestInterface -o "DestInterface Client ID: " -v @ClientId -n -o "Interface: " -v @Interface -n
-echo $CURLOUT | xmlstarlet sel -t -m //clientEntity -o "clientEntity hostName: " -v @hostName -n
-echo $CURLOUT | xmlstarlet sel -t -m //ActivePhysicalNode -o "ActivePhysicalNode hostName: " -v @hostName -n
+	CURLOUT=$(eval $CURLCMD -L $BASEURI"/Client/$CLIENTID")
+	echo $CURLOUT | xmlstarlet sel -t -m //dataInterfacePair -o "DIP Active: " -v @active -n -m SourceInterface -o "SourceInterface Client ID: " -v @ClientId -n -o "Interface: " -v @Interface -n -m ../DestInterface -o "DestInterface Client ID: " -v @ClientId -n -o "Interface: " -v @Interface -n
+	echo $CURLOUT | xmlstarlet sel -t -m //clientEntity -o "clientEntity hostName: " -v @hostName -n
+	echo $CURLOUT | xmlstarlet sel -t -m //ActivePhysicalNode -o "ActivePhysicalNode hostName: " -v @hostName -n
+}
 
-## Get client group information ##
-
-if [ "$APPNAME" != "Virtual Server" ]
-then
+get_clientgroup_info()
+{
 	echo "Client Group Name: $CLIENTGROUPNAME"
 	export CLIENTGROUPID=$($DIR/get_clientgroupid_by_clientgroupname.sh)
 	echo "Client Group ID: $CLIENTGROUPID"
-	## eval $CURLCMD -L $BASEURI"/ClientGroup/7" | xmlstarlet sel -t -m "//associatedClients[@clientName='esdc10rhl005poc']" -o "Associated Client Name: " -v @clientName -n
 	eval $CURLCMD -L $BASEURI"/ClientGroup/7" | xmlstarlet sel -t -m "//associatedClients[@clientName='"$CLIENTNAME"']" -o "Associated Client Name: " -v @clientName -n
-fi
+}
 
-## Get backupset information ##
+case "$APPNAME" in
+	"Virtual Server")
+		## Get client information ##
 
-if [ "$APPNAME" = "Windows File System" ] || [ "$APPNAME" = "Linux File System" ]
-then
-	## Get backupset by client ID ##
+		echo "Client Name: $CLIENTNAME"
+        	export CLIENTID=$($DIR/get_vsa_clientid_by_clientname.sh)
+		echo "Client ID: $CLIENTID"
+
+		## Get backupset by client ID ##
 	
-	BACKUPSET=$($DIR/get_backupset_by_clientid.sh)
-	BACKUPSETNAME=$(echo $BACKUPSET | awk -F ':' '{print $1}')
-	BACKUPSETID=$(echo $BACKUPSET | awk -F ':' '{print $2}')
-	echo "Backupset Name: $BACKUPSETNAME"
-	echo "Backupset ID: $BACKUPSETID"
-elif [ "$APPNAME" = "Virtual Server" ]
-then
-	## Get backupset by client ID ##
+		BACKUPSET=$($DIR/get_vsa_backupset_by_clientid.sh)
+		if [ -z "$BACKUPSET" ]
+		then
+			BACKUPSET=$(eval $CURLCMD -L $BASEURI"/Backupset?clientId=$CLIENTID" | xmlstarlet sel -t -m "//backupSetEntity[starts-with(@backupsetName, '$VM.201')]" -v @backupsetName -o ":" -v @backupsetId)
+		fi
+		BACKUPSETNAME=$(echo $BACKUPSET | awk -F ':' '{print $1}')
+		BACKUPSETID=$(echo $BACKUPSET | awk -F ':' '{print $2}')
+		echo "Backupset Name: $BACKUPSETNAME"
+		echo "Backupset ID: $BACKUPSETID"
 
-	BACKUPSET=$($DIR/get_vsa_backupset_by_clientid.sh)
-	if [ -z "$BACKUPSET" ]
-	then
-		BACKUPSET=$(eval $CURLCMD -L $BASEURI"/Backupset?clientId=$CLIENTID" | xmlstarlet sel -t -m "//backupSetEntity[starts-with(@backupsetName, '$VM.')]" -v @backupsetName -o ":" -v @backupsetId)
-	fi
-	BACKUPSETNAME=$(echo $BACKUPSET | awk -F ':' '{print $1}')
-	BACKUPSETID=$(echo $BACKUPSET | awk -F ':' '{print $2}')
-	echo "Backupset Name: $BACKUPSETNAME"
-	echo "Backupset ID: $BACKUPSETID"
-elif [ "$APPNAME" = "MySQL" ]
-then
-	## Get MySQL instance properties ##
+		## Get subclient information ##
 
-	eval $CURLCMD -d @- << BODY -L "$BASEURI/QCommand/qoperation%20execute" | xmlstarlet sel -t -m //instanceProperties -c instance -n -c mySqlInstance -n -c mysqlStorageDevice -n
+		SUBCLIENT=$(eval $CURLCMD -L $BASEURI"/subclient?clientId=$CLIENTID" | xmlstarlet sel -t -m "//subClientEntity[@appName='$APPNAME' and @backupsetName='$VM']" -v @subclientName -o ":"  -v @subclientId)
+		if [ -z "$SUBCLIENT" ]
+		then
+			SUBCLIENT=$(eval $CURLCMD -L $BASEURI"/subclient?clientId=$CLIENTID" | xmlstarlet sel -t -m "//subClientEntity[@appName='$APPNAME' and @backupsetName='$BACKUPSETNAME']" -v @subclientName -o ":"  -v @subclientId)
+			SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
+			SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
+		fi
+		echo "Subclient Name: $SUBCLIENTNAME"
+		echo "Subclient ID: $SUBCLIENTID"
+		if [ ! -z "$SUBCLIENTID" ]
+		then
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //dataBackupStoragePolicy -o "Data Storage policy name: " -v @storagePolicyName -n
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //children -o "VM: " -v @displayName -n
+		fi
 
-<App_GetInstancePropertiesRequest>
-	<association>
-		<entity>	
-			<clientName>$CLIENTNAME</clientName>
-			<appName>MySQL</appName> 
-			<instanceName>$CLIENTNAME</instanceName>
-		</entity>
-	</association>
-</App_GetInstancePropertiesRequest>
-BODY
+		## Get schedule policy ID by schedule policy name ##
+		
+		SCHEPID=$($DIR/get_schepid_by_schepname.sh)
+		echo "Schedule Policy ID: $SCHEPID"
+		
+		## Getting schedule policy associations ##
+		
+		eval $CURLCMD -L $BASEURI"/SchedulePolicy/$SCHEPID" | xmlstarlet sel -t -m "//associations[@subclientId='"$SUBCLIENTID"']" -o "Associated Subclient Name: " -v @subclientName -o ", Subclient ID: " -v @subclientId -n
+	;;
+	"MySQL")
+		## Get client information ##
+		get_client_info
 
-fi
+		## Get client group information ##
+		get_clientgroup_info
 
-## Get subclient information ##
+		## Get MySQL instance properties ##
+		XMLBODY="
+			<App_GetInstancePropertiesRequest>
+				<association>
+					<entity>	
+						<clientName>$CLIENTNAME</clientName>
+						<appName>MySQL</appName> 
+						<instanceName>$CLIENTNAME</instanceName>
+					</entity>
+				</association>
+			</App_GetInstancePropertiesRequest>
+		"
+		eval $CURLCMD -d \"$XMLBODY\" -L "$BASEURI/QCommand/qoperation%20execute" | xmlstarlet sel -t -m //instanceProperties -c instance -n -c mySqlInstance -n -c mysqlStorageDevice -n
 
-SUBCLIENT=$($DIR/get_subclient_by_clientid.sh)
-SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
-SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
-if [ -z "$SUBCLIENT" ] && [ "$APPNAME" = "Virtual Server" ]
-then
-	SUBCLIENT=$(eval $CURLCMD -L $BASEURI"/subclient?clientId=$CLIENTID" | xmlstarlet sel -t -m "//subClientEntity[@appName='$APPNAME' and @backupsetName='$BACKUPSETNAME']" -v @subclientName -o ":"  -v @subclientId)
-	SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
-	SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
-fi
-echo "Subclient Name: $SUBCLIENTNAME"
-echo "Subclient ID: $SUBCLIENTID"
-if [ ! -z "$SUBCLIENTID" ]
-then
-	eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //dataBackupStoragePolicy -o "Data Storage policy name: " -v @storagePolicyName -n
-	
-	if [ "$APPNAME" = "SQL Server" ]
-	then
-		eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //logBackupStoragePolicy -o "Log Storage policy name: " -v @storagePolicyName -n -m //mssqlDbContent -o "DB Name: " -v @databaseName -n
-	elif [ "$APPNAME" = "MySQL" ]
-	then
-		eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //logBackupStoragePolicy -o "Log Storage policy name: " -v @storagePolicyName -n -m //mySQLContent -o "Database: " -v @databaseName -n
-	elif [ "$APPNAME" = "Virtual Server" ]
-	then
-		eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //children -o "VM: " -v @displayName -n
-	fi
-fi
+		## Get subclient information ##
 
-## Get schedule policy ID by schedule policy name ##
+		SUBCLIENT=$(eval $CURLCMD -L $BASEURI"/subclient?clientId=$CLIENTID" | xmlstarlet sel -t -m "//subClientEntity[@appName='$APPNAME' and not(@subclientName='(command line)')]" -v @subclientName -o ":"  -v @subclientId)
+		SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
+                SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
+                echo "Subclient Name: $SUBCLIENTNAME"
+                echo "Subclient ID: $SUBCLIENTID"
+		if [ ! -z "$SUBCLIENTID" ]
+		then
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //dataBackupStoragePolicy -o "Data Storage policy name: " -v @storagePolicyName -n
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //logBackupStoragePolicy -o "Log Storage policy name: " -v @storagePolicyName -n -m //mySQLContent -o "Database: " -v @databaseName -n
+		fi
 
-SCHEPID=$($DIR/get_schepid_by_schepname.sh)
-echo "Schedule Policy ID: $SCHEPID"
+		## Get schedule policy ID by schedule policy name ##
+		
+		SCHEPID=$($DIR/get_schepid_by_schepname.sh)
+		echo "Schedule Policy ID: $SCHEPID"
+		
+		## Getting schedule policy associations ##
+		
+		eval $CURLCMD -L $BASEURI"/SchedulePolicy/$SCHEPID" | xmlstarlet sel -t -m "//associations[@subclientId='"$SUBCLIENTID"']" -o "Associated Subclient Name: " -v @subclientName -o ", Subclient ID: " -v @subclientId -n
 
-## Getting schedule policy associations ##
+		## Checking if the client is retired ##
 
-eval $CURLCMD -L $BASEURI"/SchedulePolicy/$SCHEPID" | xmlstarlet sel -t -m "//associations[@subclientId='"$SUBCLIENTID"']" -o "Associated Subclient Name: " -v @subclientName -o ", Subclient ID: " -v @subclientId -n
+		eval $CURLCMD -L $BASEURI"/Client/$CLIENTID" | xmlstarlet sel -t -m "//clientProps" -o "IsDeletedClient: " -v @IsDeletedClient -n
+	;;
+	"SQL Server")
+		## Get client information ##
+		get_client_info
 
-## Checking if the client is retired ##
+		## Get client group information ##
+		get_clientgroup_info
 
-eval $CURLCMD -L $BASEURI"/Client/$CLIENTID" | xmlstarlet sel -t -m "//clientProps" -o "IsDeletedClient: " -v @IsDeletedClient -n
+		## Get subclient information ##
+		
+		SUBCLIENT=$(eval $CURLCMD -L $BASEURI"/subclient?clientId=$CLIENTID" | xmlstarlet sel -t -m "//subClientEntity[@appName='$APPNAME' and not(@subclientName='(command line)')]" -v @subclientName -o ":"  -v @subclientId)
+		SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
+		SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
+		echo "Subclient Name: $SUBCLIENTNAME"
+		echo "Subclient ID: $SUBCLIENTID"
+		if [ ! -z "$SUBCLIENTID" ]
+		then
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //dataBackupStoragePolicy -o "Data Storage policy name: " -v @storagePolicyName -n
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //logBackupStoragePolicy -o "Log Storage policy name: " -v @storagePolicyName -n -m //mssqlDbContent -o "DB Name: " -v @databaseName -n
+		fi
+
+		## Get schedule policy ID by schedule policy name ##
+		
+		SCHEPID=$($DIR/get_schepid_by_schepname.sh)
+		echo "Schedule Policy ID: $SCHEPID"
+		
+		## Getting schedule policy associations ##
+		
+		eval $CURLCMD -L $BASEURI"/SchedulePolicy/$SCHEPID" | xmlstarlet sel -t -m "//associations[@subclientId='"$SUBCLIENTID"']" -o "Associated Subclient Name: " -v @subclientName -o ", Subclient ID: " -v @subclientId -n
+
+		## Checking if the client is retired ##
+
+		eval $CURLCMD -L $BASEURI"/Client/$CLIENTID" | xmlstarlet sel -t -m "//clientProps" -o "IsDeletedClient: " -v @IsDeletedClient -n
+	;;
+	"Windows File System"|"Linux File System")
+		## Get client information ##
+		get_client_info
+
+		## Get client group information ##
+		get_clientgroup_info
+
+		## Get backupset information ##
+
+		BACKUPSET=$($DIR/get_backupset_by_clientid.sh)
+		BACKUPSETNAME=$(echo $BACKUPSET | awk -F ':' '{print $1}')
+		BACKUPSETID=$(echo $BACKUPSET | awk -F ':' '{print $2}')
+		echo "Backupset Name: $BACKUPSETNAME"
+		echo "Backupset ID: $BACKUPSETID"
+
+		## Get subclient information ##
+		
+		SUBCLIENT=$($DIR/get_subclient_by_clientid.sh)
+		SUBCLIENTNAME=$(echo $SUBCLIENT | awk -F ':' '{print $1}')
+		SUBCLIENTID=$(echo $SUBCLIENT | awk -F ':' '{print $2}')
+		echo "Subclient Name: $SUBCLIENTNAME"
+		echo "Subclient ID: $SUBCLIENTID"
+		if [ ! -z "$SUBCLIENTID" ]
+		then
+			eval $CURLCMD -L $BASEURI"/Subclient/$SUBCLIENTID" | xmlstarlet sel -t -m //dataBackupStoragePolicy -o "Data Storage policy name: " -v @storagePolicyName -n
+		fi
+
+		## Get schedule policy ID by schedule policy name ##
+		
+		SCHEPID=$($DIR/get_schepid_by_schepname.sh)
+		echo "Schedule Policy ID: $SCHEPID"
+		
+		## Getting schedule policy associations ##
+		
+		eval $CURLCMD -L $BASEURI"/SchedulePolicy/$SCHEPID" | xmlstarlet sel -t -m "//associations[@subclientId='"$SUBCLIENTID"']" -o "Associated Subclient Name: " -v @subclientName -o ", Subclient ID: " -v @subclientId -n
+
+		## Checking if the client is retired ##
+
+		eval $CURLCMD -L $BASEURI"/Client/$CLIENTID" | xmlstarlet sel -t -m "//clientProps" -o "IsDeletedClient: " -v @IsDeletedClient -n
+	;;
+esac
 
 ## Logout ##
 
